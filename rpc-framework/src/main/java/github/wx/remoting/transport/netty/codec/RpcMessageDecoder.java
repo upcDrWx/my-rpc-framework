f98115a6c82bcdaf3d57621915f4306e1a6d5faf
WangXin <18219637312@163.com>
2023-10-05 21:57:43 +0800

    修改解码出错的bug

--- rpc-framework/src/main/java/github/wx/remoting/transport/netty/codec/RpcMessageDecoder.java
+++ rpc-framework/src/main/java/github/wx/remoting/transport/netty/codec/RpcMessageDecoder.java
@@ -122,8 +122,7 @@ public class RpcMessageDecoder extends LengthFieldBasedFrameDecoder {
             // 反序列化
             String codecName = SerializationTypeEnum.getName(rpcMessage.getCodec());
             log.info("解码名称: [{}] ", codecName);
-//            Serializer serializer = ExtensionLoader.getExtensionLoader(Serializer.class).getExtension(codecName);
-            Serializer serializer = null;
+            Serializer serializer = ExtensionLoader.getExtensionLoader(Serializer.class).getExtension(codecName);
             if (messageType == RpcConstants.REQUEST_TYPE) {
                 RpcRequest tmpValue = serializer.deserialize(bs, RpcRequest.class);
                 rpcMessage.setData(tmpValue);
--- rpc-framework/src/main/java/github/wx/remoting/transport/netty/test/serializer/SerializeException.java
+++
@@ -1,7 +0,0 @@
-package github.wx.remoting.transport.netty.test.serializer;
-
-public class SerializeException extends RuntimeException {
-    public SerializeException(String message) {
-        super(message);
-    }
-}
